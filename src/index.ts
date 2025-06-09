5222a48b9ad59f01c52c0eda923cf9fc6b2ad3ce
Dan Neumann <danrodneu@gmail.com>
2025-06-09 09:35:10 -0500

    expose tokenizer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from "./parser.js";
+export {
+  createParser,
+  type StyledText,
+  type Parser,
+  type Color,
+  type Decoration,
+} from "./parser.js";
+
+export { createTokenizer, type Tokenizer, type Token } from "./tokenizer.js";
